3423c92c4c52ead2f73643fb96b750018f7c196b
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-08 23:17:17 +0100

    enable SW

--- pjdreact/src/index.js
+++ pjdreact/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Components/index.css';
 import Main from './Components/Main';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
-
+serviceWorker.register();
