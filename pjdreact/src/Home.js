5d7f27b79bcdf363f0ada3b5c694adae2c45eab5
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-08 21:06:56 +0100

    BASIC SITE WORKING:)

---
+++ pjdreact/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello.</h2>
+        <p>My name is Patrick Davis, an aspiring web developer from Bournemouth.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
