0815d809182f2314f44725d1070dc0d207835908
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-10 11:41:24 +0100

    build

---
+++ build/precache-manifest.a92752f659077c73838959704927c187.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d45e898d89f337ebfb13fc16c84cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d12a6d6e8225d131f6",
+    "url": "/static/css/main.0aedcc8a.chunk.css"
+  },
+  {
+    "revision": "cc0163fcf1cca8c65326",
+    "url": "/static/js/2.4a555f25.chunk.js"
+  },
+  {
+    "revision": "e4d12a6d6e8225d131f6",
+    "url": "/static/js/main.f440de24.chunk.js"
+  },
+  {
+    "revision": "8150fdc4cf072fb5ef7b",
+    "url": "/static/js/runtime-main.a94134b0.js"
+  },
+  {
+    "revision": "b727621d93d8b2ec501e15c81e1b1c5e",
+    "url": "/static/media/Apostrophe.b727621d.jpg"
+  },
+  {
+    "revision": "5975c546f5bed30759a290b375bcdb32",
+    "url": "/static/media/ChloeTextiles.5975c546.jpg"
+  },
+  {
+    "revision": "5866dcf7665782aa9726e74c55b28361",
+    "url": "/static/media/Code.5866dcf7.jpg"
+  },
+  {
+    "revision": "ffc3aaf6ab6434bf74641e09f2305cd2",
+    "url": "/static/media/CodeMac.ffc3aaf6.jpg"
+  },
+  {
+    "revision": "809ea306ee560109de72c6203184a03d",
+    "url": "/static/media/Empaths.809ea306.jpg"
+  },
+  {
+    "revision": "0f466170759d4fd93e7dc4fb99a681da",
+    "url": "/static/media/JulieArt.0f466170.jpg"
+  },
+  {
+    "revision": "3b204fa88344566d4231c538ea707582",
+    "url": "/static/media/Me.3b204fa8.jpg"
+  }
+]);
