43d8735922ffdd3ed50bc75ebbe140604c36a8ba
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-10 12:23:40 +0100

    form fix?

--- src/Components/ContactPage/ContactForm/ContactForm.js
+++ src/Components/ContactPage/ContactForm/ContactForm.js
@@ -10,7 +10,7 @@ class ContactForm extends React.Component {
                         <h3 className="mb-0">Let's talk</h3>
                     </div>
                     <div className="card-body">
-                        <form className="form" name="contact" method="POST" data-netlify="true" action="https://www.patrickdavis.co.uk/#/contact">
+                        <form className="form" name="contact" data-netlify="true">
                             <p class="hidden">
                                 <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
                             </p>
