f37533120e578df02fcf16b7037ddebcf0984b20
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-10 11:52:46 +0100

    honeypot form add and name form

--- src/Components/ContactPage/ContactForm/ContactForm.js
+++ src/Components/ContactPage/ContactForm/ContactForm.js
@@ -3,43 +3,46 @@ import React from "react";
 class ContactForm extends React.Component {
     render() {
         return (
-        <div>
-         <h1>Contact</h1>
-            <div className="card card-outline-secondary">
-                <div className="card-header">
-                    <h3 className="mb-0">Let's talk</h3>
-                </div>
-                <div className="card-body">
-                    <form className="form" method="POST" data-netlify="true">
-                        <fieldset>
-                            <label htmlFor="name2" className="mb-0">Name*</label>
-                            <div className="row mb-1">
-                                <div className="col-lg-12">
-                                    <input type="text" name="name2" id="name2" className="form-control" required />
+            <div>
+                <h1>Contact</h1>
+                <div className="card card-outline-secondary">
+                    <div className="card-header">
+                        <h3 className="mb-0">Let's talk</h3>
+                    </div>
+                    <div className="card-body">
+                        <form className="form" name="contact" method="POST" data-netlify="true">
+                            <p class="hidden">
+                                <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+                            </p>
+                            <fieldset>
+                                <label htmlFor="name2" className="mb-0">Name*</label>
+                                <div className="row mb-1">
+                                    <div className="col-lg-12">
+                                        <input type="text" name="name2" id="name2" className="form-control" required />
+                                    </div>
                                 </div>
-                            </div>
-                            <label htmlFor="email2" className="mb-0">Email*</label>
-                            <div className="row mb-1">
-                                <div className="col-lg-12">
-                                    <input type="email" name="email2" id="email2" className="form-control" required />
+                                <label htmlFor="email2" className="mb-0">Email*</label>
+                                <div className="row mb-1">
+                                    <div className="col-lg-12">
+                                        <input type="email" name="email2" id="email2" className="form-control" required />
+                                    </div>
                                 </div>
-                            </div>
-                            <label htmlFor="message2" className="mb-0">Message*</label>
-                            <div className="row mb-1">
-                                <div className="col-lg-12">
-                                    <textarea rows="6" name="message2" id="message2" className="form-control" required></textarea>
+                                <label htmlFor="message2" className="mb-0">Message*</label>
+                                <div className="row mb-1">
+                                    <div className="col-lg-12">
+                                        <textarea rows="6" name="message2" id="message2" className="form-control" required></textarea>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="form-row text-center">
-                                <div className="col-12">
-                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                <div className="form-row text-center">
+                                    <div className="col-12">
+                                        <button type="submit" className="btn btn-primary">Submit</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </fieldset>
-                    </form>
+                            </fieldset>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 }
