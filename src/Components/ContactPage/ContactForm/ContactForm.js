2bc64851579579aa679dc6fb93bb013edc3243a9
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-15 10:21:06 +0100

    add contact form back in

--- src/Components/ContactPage/ContactForm/ContactForm.js
+++ src/Components/ContactPage/ContactForm/ContactForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Obfuscate from 'react-obfuscate';
 class ContactForm extends React.Component {
     render() {
         return (
@@ -7,13 +6,23 @@ class ContactForm extends React.Component {
                 <div className="card card-outline-secondary center">
                     <h1 center>Contact Me</h1>
                     <div className="card-header center">
-                        <Obfuscate
-                            className="acustom btn btn-primary"
-                            email="hello@patrickdavis.co.uk"
-                            headers={{
-                                subject: 'Question from your website'
-                            }}
-                        />
+                        <form action="https://formspree.io/hello@patrickdavis.co.uk" method="POST">
+                            <div class="mx-auto form-group col-lg-6">
+                                <label class="form-control-label" for="form-group-input">Name*</label>
+                                <input required type="text" class="form-control" id="form-group-input" name="Name" />
+                            </div>
+                            <div class="form-group mx-auto col-lg-6 ">
+                                <label class="form-control-label" for="form-group-input">Email*</label>
+                                <input required type="email" class="form-control" id="form-group-input" name="Email" />
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <label class="form-control-label" for="form-group-input">Message*</label>
+                                <textarea required class="form-control" id="form-group-input" name="Message" rows="6"></textarea>
+                            </div>
+                            <div>
+                                <input className="btn btn-primary btn-large" value="Submit" type="submit" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
