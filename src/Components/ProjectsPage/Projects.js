ada0a44c9dcbb3aa5e77dcdf500152f9833226ad
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-19 18:06:47 +0100

    button change

--- src/Components/ProjectsPage/Projects.js
+++ src/Components/ProjectsPage/Projects.js
@@ -24,7 +24,7 @@ class Stuff extends React.Component {
                     <p className="card-text mx-3">I included Fontawesome & some of hvr.css for further styling.</p>
                     <p className="card-text mx-3">The site is served statically for the ultimate speed and caching policy.</p>
                     <p className="card-text mx-3">The motivation for this site help out an upcoming music duo increase brand awareness and have a responsive and smooth digital platform to communicate through.</p>
-                    <p className="card-text mx-3"><small className="text-muted">Released October 2019 | <a title="Link to site" target="_blank" rel="noopener noreferrer" href="https://www.empathsofficial.com">Link To Site</a> </small></p>
+                    <p className="card-text mx-3"><small className="text-muted">Released October 2019 | <a className="btn btn-primary btn-sm" title="Link to site" target="_blank" rel="noopener noreferrer" href="https://www.empathsofficial.com">Link To Site</a> </small></p>
                   </div>
                 </div>
               </div>
@@ -40,7 +40,7 @@ class Stuff extends React.Component {
                     <p className="card-text mx-3">
                       This is an Apostrophe CMS Skeleton Kit that I released using Bootstrap 4. Check it out! I figured it'd help someone starting out with the CMS, which is useful for some bespoke projects.</p>
                     <p className="card-text mx-3">The community behind the CMS is quite small and it isn't something you'd see in industry, but it's relatively simple to use and could power quite basic sites.</p>
-                    <p className="card-text mx-3"><small className="text-muted">September 2019 | <a title="Link to Repo" target="_blank" rel="noopener noreferrer" href="https://github.com/patrickd77-eng/ApostropheCMSBootstrap4Skeleton">Link To Repo</a> </small></p>
+                    <p className="card-text mx-3"><small className="text-muted">September 2019 | <a className="btn btn-primary btn-sm" title="Link to Repo" target="_blank" rel="noopener noreferrer" href="https://github.com/patrickd77-eng/ApostropheCMSBootstrap4Skeleton">Link To Repo</a> </small></p>
                   </div>
                 </div>
               </div>
@@ -56,7 +56,7 @@ class Stuff extends React.Component {
                     <p className="card-text mx-3">
                       This is a Bootstrap 4 blog template that I released using the Django Framework. Check it out! I figured it'd help someone starting out with django, which is useful for quick projects using authentication.</p>
                     <p className="card-text mx-3">Because this is a template repo with instructions, it's very easy to be used or developed on. With some extra styling, a fully functional CRUD capable blog could be made.</p>
-                    <p className="card-text mx-3"><small className="text-muted">September 2019 | <a title="Link to Repo" target="_blank" rel="noopener noreferrer" href="https://github.com/patrickd77-eng/DjangoBlogStarter">Link To Repo</a> </small></p>
+                    <p className="card-text mx-3"><small className="text-muted">September 2019 | <a className="btn btn-primary btn-sm" title="Link to Repo" target="_blank" rel="noopener noreferrer" href="https://github.com/patrickd77-eng/DjangoBlogStarter">Link To Repo</a> </small></p>
                   </div>
                 </div>
               </div>
