baebd9412e81557da925dbf8e0bd50a8273d4eb3
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-10-22 10:01:53 +0100

    form changes

--- src/Components/ContactPage/ContactForm/ContactForm.js
+++ src/Components/ContactPage/ContactForm/ContactForm.js
@@ -8,15 +8,15 @@ class ContactForm extends React.Component {
                     <div className="card-header center">
                         <form action="https://formspree.io/hello@patrickdavis.co.uk" method="POST">
                             <div class="mx-auto form-group col-lg-6">
-                                <label class="form-control-label" for="form-group-input">Name*</label>
+                                <label class="form-control-label" for="form-group-input"><strong>Name*</strong></label>
                                 <input required type="text" class="form-control" id="form-group-input" name="Name" />
                             </div>
                             <div class="form-group mx-auto col-lg-6 ">
-                                <label class="form-control-label" for="form-group-input">Email*</label>
+                                <label class="form-control-label" for="form-group-input"><strong>Email*</strong></label>
                                 <input required type="email" class="form-control" id="form-group-input" name="Email" />
                             </div>
-                            <div class="form-group col-lg-12">
-                                <label class="form-control-label" for="form-group-input">Message*</label>
+                            <div class="form-group col-lg-8">
+                                <label class="form-control-label" for="form-group-input"><strong>Message*</strong></label>
                                 <textarea required class="form-control" id="form-group-input" name="Message" rows="6"></textarea>
                             </div>
                             <div>
